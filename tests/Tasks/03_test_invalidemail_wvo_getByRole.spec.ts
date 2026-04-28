c0de41755607c570a04fee45d5ab18f61cfcbe0a
Pooja Tiwari <pooja.tiwari@techverito.com>
2026-04-28 22:59:41 +0530

    Task: Added test to use getByRole for invalid username scenario

--- tests/Tasks/03_test_invalidemail_wvo_getByRole.spec.ts
+++ tests/Tasks/03_test_invalidemail_wvo_getByRole.spec.ts
@@ -3,15 +3,20 @@ import { test , expect } from '@playwright/test'
 test('Verify error message for invalid email on app.vwo.com', async ( { page}) => {
 
     await page.goto("https://app.vwo.com/#/login");
-    await page.locator("#login-username").fill("poojakt123");
-    await page.locator("#login-password").fill("pass@123");
+    await page.getByRole('textbox' , {name : 'Email address'}).fill("poojakt123");
+    await page.getByRole('textbox' , {name : 'Password'}).fill("pass@123");
     await page.locator("li[data-qa='wusegasoju'] span.checkbox-radio-button.ng-scope").check();
-    await page.locator("#js-login-btn").click();
-    const actualErrorMessage = await page.locator("#js-notification-box-msg").textContent();
-    await page.waitForTimeout(1000)
-    console.log("actualErrorMessage : ", actualErrorMessage);
-    const expectedErrorMessage = "Your email, password, IP address or location did not match";
-    // expect(actualErrorMessage).toEqual(expectedErrorMessage);
-    expect(page.locator("#js-notification-box-msg")).toHaveText("Your email, password, IP address or location did not match");
+    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+
+    // WORKING
+    await expect(page.locator("#js-notification-box-msg")).toBeVisible();
+    await expect(page.locator("#js-notification-box-msg")).toHaveText("Your email, password, IP address or location did not match");
+    // await expect(page.getByText("Your email, password, IP address or location did not match")).toBeVisible();
+    
+
+    // NOT WORKING
+    // await expect(page.getByTestId('rixawilomi')).toBeVisible();
+    // await expect(page.getByTestId('rixawilomi')).toHaveText('Your email, password, IP address or location did not match');
+    
     
 })
