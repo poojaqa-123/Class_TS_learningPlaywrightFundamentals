443c3b2042198f75ab67745fb685d0862ff33ebb
Pooja Tiwari <pooja.tiwari@techverito.com>
2026-05-05 14:25:17 +0530

    Project 01: Added test script for TTABank signup transfer amout verify remaing balance.

---
+++ tests/Projects/01_Project_TTA_Bank/test_signup.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test'
+
+test('Sign up in TTA bank website', async ({ page }) => {
+
+    await page.goto("https://tta-bank-digital-973242068062.us-west1.run.app/");
+    // await page.getByText("Sign Up").click();
+    await page.getByRole('button', { name: 'Sign Up' }).click();
+    await page.getByPlaceholder("John Doe").fill("Pooja");
+    await page.getByPlaceholder("you@example.com").fill("poojakt123@gmail.com");
+    await page.getByPlaceholder("••••••••").fill("pass123");
+    await page.getByRole('button', { name: 'Create Account'}).click();
+    await expect(page.locator("//span[@class='text-2xl font-bold tracking-tight' and text()='TTA Bank']")).toBeVisible();
+
+    const balnceStr = await page.locator("//h3[@class='mt-2 text-3xl font-bold']").textContent() ?? '0';
+    const totalBalance = parseFloat(balnceStr.replace(/[^0-9.-]+/g, ""));
+    console.log(" Total balance : ", totalBalance);
+
+    await page.getByRole('button', { name : 'Transfer Funds'}).click();
+    await page.getByPlaceholder("0.00").fill("5000");
+    
+    // const inputLocator = page.locator('input[type="number"]');
+    // const rent_value = await inputLocator.inputValue();
+    const rentStr = await page.locator('input[type="number"]').inputValue() ?? '0';
+    const rentValue = parseFloat(rentStr.replace(/[^0-9.-]+/g, ""));
+
+
+    console.log(" Rent : ", rentValue);
+
+    await page.getByPlaceholder("e.g. Rent for October").fill("Rent"); 
+    await page.getByText("Continue").click();
+    
+    await expect(page.getByText("Review Transfer")).toBeVisible();
+    await page.getByText("Confirm Transfer").click();
+    
+    await page.getByRole('button' , { name : 'Dashboard'}).click();
+
+    const remainingBalance : number = totalBalance - rentValue ;
+    console.log(" Remainig balance : ", remainingBalance);
+
+    expect(remainingBalance).toEqual(45000);
+
+})
